456f318ceb5e14aad44328970dce1b6b27828dc0
Jegan Amalasingam <jegan_arockiaraj@apple.com>
2019-10-05 22:41:20 -0700

    class condition and more

--- src/components/day.js
+++ src/components/day.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { getDate } from 'date-fns'
+import './day.scss';
 
-const day = (date,seleced, disabled, onClick,  ) => {
-
-    const selectedClass = seleced ? 'selected-day' : '';
+const day = ({date,selected, disabled, onClick}) => {
     
+    
+    
+    const classNameList = ['day'];
+    
+    if (selected) {
+        classNameList.push('selected-day');
+    }
+    const classNames = classNameList.join(' ');
+
     const handleClick = ()=> onClick(date);
      return (
-         <div onClick={handleClick}>
+         <div className={classNames} onClick={handleClick}>
             {getDate(date)}
          </div>
      )
 }
-
 export default day;
-
---
+++ src/stories/1-day.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Day from '../components/Day';
+
+export default {
+  title: 'Day',
+};
+
+const date = new Date();
+
+
+export const day= () => <Day date={date} onClick={action('clicked')}/>;
+
+export const selectedDay= () => <Day date={date} selected={true} onClick={action('clicked')}/>;
+
