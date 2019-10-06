7c44470debb056fbe1ea27c326f79a1190455957
Jegan <a.jegan@gmail.com>
2019-10-05 23:37:19 -0700

    formatting

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import { getDate } from 'date-fns'
-import './day.scss';
+import React from "react";
+import { getDate } from "date-fns";
+import "./day.scss";
 
-const day = ({date,selected, disabled, onClick}) => {
-    
-    
-    
-    const classNameList = ['day'];
-    
+const day = ({ date, selected, disabled, onClick }) => {
+    const classNameList = ["day"];
+
     if (selected) {
-        classNameList.push('selected-day');
+        classNameList.push("selected-day");
     }
     if (disabled) {
-        classNameList.push('disabled-day');
+        classNameList.push("disabled-day");
     }
-    const classNames = classNameList.join(' ');
+    const classNames = classNameList.join(" ");
 
-    const handleClick = ()=> onClick(date);
-     return (
-         <div className={classNames} onClick={handleClick}>
+    const handleClick = () => onClick(date);
+    return (
+        <div className={classNames} onClick={handleClick}>
             {getDate(date)}
-         </div>
-     )
-}
+        </div>
+    );
+};
 export default day;
